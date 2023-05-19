360b814c47c3b2c6e044f608120ad844698253f7
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-05-19 20:01:02 +0800

    day08代码：面向对象练习1

---
+++ JavaSE/src/com/hitech/day08_code/test1/Phone.java
@@ -0,0 +1,14 @@
+package com.hitech.day08_code.test1;
+
+public class Phone {
+    String brand;
+    double price;
+
+    public void call() {
+        System.out.println("手机在打电话");
+    }
+
+    public void playGame() {
+        System.out.println("手机在玩游戏");
+    }
+}
