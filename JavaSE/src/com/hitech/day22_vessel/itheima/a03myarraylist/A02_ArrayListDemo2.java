e467404c4f704fbaf59aa3c858af246064774afb
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-06-01 23:11:02 +0800

    day22代码: 集合进阶练习1->List集合

---
+++ JavaSE/src/com/hitech/day22_vessel/itheima/a03myarraylist/A02_ArrayListDemo2.java
@@ -0,0 +1,13 @@
+package com.hitech.day22_vessel.itheima.a03myarraylist;
+
+public class A02_ArrayListDemo2 {
+    public static void main(String[] args) {
+
+
+
+
+
+
+    }
+
+}
---
+++ JavaSE/src/com/hitech/day22_vessel/itheima/a05test/Dog.java
@@ -0,0 +1,4 @@
+package com.hitech.day22_vessel.itheima.a05test;
+
+public abstract class Dog extends Animal{
+}
