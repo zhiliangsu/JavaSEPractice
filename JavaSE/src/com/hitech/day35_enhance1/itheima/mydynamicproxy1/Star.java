f4992556df1fd5098ecca18e7b0f282d446c64fa
zhiliangsu <zhiliangsuzhiliang@gmail.com>
2023-06-15 16:47:39 +0800

    day35代码: 反射&动态代理练习

---
+++ JavaSE/src/com/hitech/day35_enhance1/itheima/mydynamicproxy1/Star.java
@@ -0,0 +1,14 @@
+package com.hitech.day35_enhance1.itheima.mydynamicproxy1;
+
+public interface Star {
+
+    //我们可以把所有想要被代理的方法定义在接口当中
+
+    //唱歌
+    public abstract String sing(String name);
+
+    //跳舞
+    public abstract void dance();
+
+
+}
